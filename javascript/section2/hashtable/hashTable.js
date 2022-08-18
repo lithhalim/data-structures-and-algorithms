8c5c210a422b4fb7fe93300ad800283be0e0be49
lithhalim <lithhalim@outlook.com>
2022-08-18 13:11:03 +0300

    d

---
+++ javascript/section2/hashtable/hashTable.js
@@ -0,0 +1,103 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+    // returning the all the values  from the linkedList
+    values() {
+        let values = [];
+        let current = this.head;
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        return values;
+    }
+}
+
+class Hashmap {
+    constructor(size) {
+        //specific the size Of the array
+        this.size = size;
+        //create Array with the specific array size
+        this.map = new Array(size);
+    }
+    //create the hashing for the key
+    makeHash(key) {
+        //use ascii --->lith-->11+15+20+10-->the summation for key string
+        const asciicodeSum = key.split("").reduce((acc, cur) => {
+            return acc + cur.charCodeAt(0);
+        }, 0);
+        //multible the summation ascii with primery key
+        const multiPrime = asciicodeSum * 599;
+        //get the module is the index create
+        const theIndex = multiPrime % this.size;
+        return theIndex;
+    }
+    add(key, value) {
+        //add elemnt first hash key to insert the elemnt in specific index
+        const hash = this.makeHash(key);
+        //create empty object to insert the key and value
+        if (!this.map[hash]) {
+            this.map[hash] = new LinkedList();
+        }
+        //[key] to can insert key with ununiform style
+        this.map[hash].append({ [key]: value });
+    }
+
+    get(index){
+        let output=[]
+        let current=myhashmap.map[index].head;
+        while(current){
+            output.push(current.value)
+            current=current.next
+        }
+        return console.log(output)
+    }
+    containes(index){
+        if(myhashmap.map[index]==undefined){
+            return console.log(false)
+        }
+        else {
+            return true
+        }
+    }
+    keys(){
+        let output=[];
+        myhashmap.map.forEach((current)=>{
+            output.push(Object.keys(current.head.value)[0])
+            if(current.head.next!==null){
+                output.push(Object.keys(current.head.next.value)[0])
+            }
+        })
+        return console.log(output)
+    }
+}
+
+const myhashmap = new Hashmap(5);
+myhashmap.add('lithh', '545454');
+myhashmap.add('ahmed', '545454');
+myhashmap.add('khalid', '412');
+myhashmap.add('sara', '8787');
+myhashmap.add('lithh', '545454');
+
+myhashmap.containes(1)
+
+myhashmap.keys()
